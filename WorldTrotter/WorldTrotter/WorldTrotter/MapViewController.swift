fb9efe4d3267a2dd6eb19f28f61c067e7bac2abf
Crystal Jade Allen-Washington <crystaljade@Macbooks-Air.lan>
2017-05-15 12:11:52 -0400

    Second commit - World Trotter app complete.

--- WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -14,27 +14,46 @@ class MapViewController: UIViewController {
     override func loadView() {
         mapView = MKMapView()
         view = mapView
-    
-    let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
-    segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
-    segmentedControl.selectedSegmentIndex = 0
-    
-    segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(segmentedControl)
         
-    let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
-    let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-    let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-    
-    topConstraint.isActive = true
-    leadingConstraint.isActive = true
-    trailingConstraint.isActive = true
+        let standardString = NSLocalizedString("Standard", comment: "Standard map view")
+        let satelliteString = NSLocalizedString("Satellite", comment: "Satellite map view")
+        let hybridString = NSLocalizedString("Hybrid", comment: "Hybrid map view")
+        let segmentedControl = UISegmentedControl(items: [standardString, satelliteString, hybridString])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
         
     }
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("MapViewController loaded its view.")
+    }
+    
+    func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
     }
 }
