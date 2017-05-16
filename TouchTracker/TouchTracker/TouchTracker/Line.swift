f688039f9413809c915ef23cc56d94f5ea618714
Crystal Jade Allen-Washington <crystaljade@Macbooks-Air.lan>
2017-05-16 14:01:41 -0400

    TouchTracker complete (BNR Guide complete through Chapter 18.)

---
+++ TouchTracker/TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Crystal Jade Allen-Washington on 5/16/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
