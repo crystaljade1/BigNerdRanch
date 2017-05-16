eaf8b4454422c9cd1ec92afc09fb0259a59f0495
Crystal Jade Allen-Washington <crystaljade@Macbooks-Air.lan>
2017-05-16 09:38:29 -0400

    Commit #4 - Chapter 14 in progress.

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Crystal Jade Allen-Washington on 5/15/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
